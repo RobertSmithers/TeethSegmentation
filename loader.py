35c70c6a160c067ad159415e980a52cf22ad4f9a
Yuting Ji <seizetimejay@gmail.com>
2022-04-24 01:02:13 -0400

    Updated Loader.py and sample.csv

--- loader.py
+++ loader.py
@@ -1,7 +1,10 @@
+import os
+import random
 import numpy as np
 import pandas as pd
 import cv2
-import os
+from PIL import Image
+
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
@@ -12,10 +15,8 @@ import torchvision.transforms.functional as TF
 import torch.nn as nn
 import torch.nn.functional as F
 
-from PIL import Image
 
-###Pre-Process DATA###
-
+###Pre-Process Images###
 def convert_one_channel(img):
     #some images have 3 channels , although they are grayscale image
     if len(img.shape)>2:
@@ -28,6 +29,7 @@ def pre_images(resize_shape,path):
     img=Image.open(path)
     img=img.resize((resize_shape),Image.ANTIALIAS)
     img=convert_one_channel(np.asarray(img))
+    cv2.imwrite(path,img)
     return img
 
 def show_imgs():
@@ -36,27 +38,54 @@ def show_imgs():
       file_path1 = os.path.join('original_img', str(index+1)+'.png')
       file_path2 = os.path.join('masked_img', str(index+1)+'.png')
       # print(file_path)
-      ax = fig.add_subplot(2, 7, index+1) 
+      ax = fig.add_subplot(2, 7, index+1)
       plt.imshow(pre_images((512,512),file_path1))  #show result of converting every img to one color channel
-        
+
       ax = fig.add_subplot(2, 7, index+8)
       plt.imshow(cv2.imread(file_path2))
-      
+
+
+def gen_csv():
+    ###generate file names to keep track of file for later usage
+    arr1=np.arange(1,117)
+    # print(arr1.dtype)
+    arr1=arr1.astype(str)
+    # print(type(arr1))
+    df=pd.DataFrame(arr1)
+    df[1]=df[0]
+    df[0]=df[0]+'.png'
+    df.to_csv('data/sample.csv',index=False)
 ###Load DATA
+
+def rename():
+    ###rename all files in masked_img folder
+    folder='data/masked_img'
+    for file_name in os.listdir(folder):
+        source = folder+'/'+file_name
+        destination = source.replace('_m.png','.png')
+        os.rename(source, destination)
+
+def mergefiles():
+    ###move renamed files to Images folder
+    folder='data/masked_img'
+    for file_name in os.listdir(folder):
+      source = folder+'/'+file_name
+      destination = 'data/original_img/'+file_name
+      os.rename(source,destination)
 
 # our dataset class
 rest_set_size = 0.3
 test_set_size = 0.5
 class dset(Dataset):
     def __init__(self, root_dir='data', train=True,test=True,transformX = None, transformY = None):
-      
+
       try:
         self.pixel_file = pd.read_csv(os.path.join(root_dir, 'sample.csv'))
       except:
         df = pd.DataFrame(np.arange(1,117))
         df.to_csv(os.path.join(root_dir,'sample.csv'))
         self.pixel_file = pd.read_csv(os.path.join(root_dir, 'sample.csv'))
-        
+
       self.root_dir = root_dir
       self.transformX = transformX
       self.transformY = transformY
@@ -76,28 +105,46 @@ class dset(Dataset):
         else:
           length = len(self.validation_data)
         return length
-    
+
     def __getitem__(self, index):
         if self.train:
-          imx_name = os.path.join(self.root_dir, 'original_img', f'{self.train_data.iloc[index, 0]}.png')
-          imy_name = os.path.join(self.root_dir, 'masked_img', f'{self.train_data.iloc[index, 0]}.png')
+          imx_name = os.path.join(self.root_dir, 'original_img',self.train_data.iloc[index, 0])
+          imy_name = os.path.join(self.root_dir, 'original_img',self.train_data.iloc[index, 0].replace('.png','_m.png'))
         elif self.test:
-          imx_name = os.path.join(self.root_dir, 'original_img', f'{self.test_data.iloc[index, 0]}.png')
-          imy_name = os.path.join(self.root_dir, 'masked_img', f'{self.test_data.iloc[index, 0]}.png')
+          imx_name = os.path.join(self.root_dir, 'original_img',self.test_data.iloc[index, 0])
+          imy_name = os.path.join(self.root_dir, 'original_img',self.test_data.iloc[index, 0].replace('.png','_m.png'))
         else:
-          imx_name = os.path.join(self.root_dir, 'original_img', f'{self.validation_data.iloc[index, 0]}.png')
-          imy_name = os.path.join(self.root_dir, 'masked_img', f'{self.validation_data.iloc[index, 0]}.png')
-        
+          imx_name = os.path.join(self.root_dir, 'original_img', self.validation_data.iloc[index, 0])
+          imy_name = os.path.join(self.root_dir, 'original_img',self.validation_data.iloc[index, 0].replace('.png','_m.png'))
+
         imx = Image.open(imx_name)
         imy = Image.open(imy_name).convert('L')
+
+        ##data augmentation
+        if self.train:
+          #Random horizontal flipping
+          if random.random() > 0.5:
+              imx = TF.hflip(imx)
+              imy = TF.hflip(imy)
+
+          #Random vertical flipping
+          if random.random() > 0.5:
+              imx = TF.vflip(imx)
+              imy = TF.vflip(imy)
+
+          #Random rotation
+          if random.random() > 0.8:
+            angle = random.choice([-30, -90, -60, -45 -15, 0, 15, 30, 45, 60, 90])
+            imx = TF.rotate(imx, angle)
+            imy = TF.rotate(imy, angle)
 
         if self.transformX:
             imx = self.transformX(imx)
             imy = self.transformY(imy)
-      
+
         sample = {'image': imx, 'annotation': imy}
         return sample
-    
+
 tx_X = transforms.Compose([ transforms.Resize((512, 512)),
                               transforms.ToTensor(),
                               transforms.Normalize((0.5,), (0.5,))
