17b8da675b90b5a267fc54742266ad7d328809c8
RJ <rjsmithers3@gmail.com>
2022-05-11 12:19:42 -0400

    Added better aug, big fixes on model shortcuts, out of mem :(

--- model.py
+++ model.py
@@ -12,25 +12,27 @@ class DoubleConvBlock(nn.Module):
 
     if batch_norm:
       self.conv = nn.Sequential(
-          nn.Conv2d(in_ch, out_ch // 2, 3, padding="same"),
+          nn.Conv2d(in_ch, out_ch, 3, padding="same"),
           nn.Dropout2d(0.1),
-          nn.ReLU(inplace=True),
-          nn.Conv2d(out_ch // 2, out_ch, 3, padding="same"),
+          nn.BatchNorm2d(out_ch),
+          nn.LeakyReLU(inplace=True),
+          nn.Conv2d(out_ch, out_ch, 3, padding="same"),
           nn.BatchNorm2d(out_ch),             # Heavily debated, but have decided to normalize before activation
-          nn.ReLU(inplace=True)
+          nn.LeakyReLU(inplace=True)
       )
     else:
       self.conv = nn.Sequential(
-          nn.Conv2d(in_ch, out_ch // 2, 3, padding="same"),
+          nn.Conv2d(in_ch, out_ch, 3, padding="same"),
           nn.Dropout2d(0.1),
-          nn.ReLU(inplace=True),
-          nn.Conv2d(out_ch // 2, out_ch, 3, padding="same"),
-          nn.ReLU(inplace=True)
+          nn.LeakyReLU(inplace=True),
+          nn.Conv2d(out_ch, out_ch, 3, padding="same"),
+          nn.LeakyReLU(inplace=True)
       )
 
   def forward(self, x):
       return self.conv(x)
 
+# This is our encoder
 class PoolConvBlock(nn.Module):
   # (Pool) => (DoubleConvBlock)
 
@@ -40,8 +42,9 @@ class PoolConvBlock(nn.Module):
     self.conv = DoubleConvBlock(conv_in_ch, conv_out_ch, conv_kernel_size, batch_norm=batch_norm)
 
   def forward(self, x):
-    x = self.pool(x)
-    return self.conv(x)
+    x = self.conv(x)
+    p = self.pool(x)
+    return x, p
 
 class TConv(nn.Module):
   # (ConvTranspose) => (ReLU)
@@ -50,11 +53,13 @@ class TConv(nn.Module):
     super(TConv, self).__init__()
     self.conv = nn.Sequential(
           nn.ConvTranspose2d(in_ch, out_ch, kernel_size, padding=1, stride=stride),
-          nn.ReLU(inplace=True)
+          # nn.LeakyReLU(inplace=True)
       )
   
-  def forward(self, x):
-    return self.conv(x)
+  def forward(self, x, skip):
+    x = self.conv(x)
+    x = torch.cat([x, skip], axis=1)
+    return x
 
 class UNet(nn.Module):
     def __init__(self, input_shape=(512,512,1)):
@@ -102,7 +107,7 @@ class UNet(nn.Module):
         # d6=Dropout(0.4)(conv12)
         # conv13 = Conv2D(256,(3,3), activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(d6)
         # b5=BatchNormalization()(conv13)
-        self.conv1 = DoubleConvBlock(256, 256, (3,3))
+        self.conv1 = DoubleConvBlock(512, 256, (3,3))
         
         # conv14 = Conv2DTranspose(256,(4,4), activation = 'relu', padding = 'same', strides=(2,2),kernel_initializer = 'he_normal')(b5)
         self.tconv2 = TConv(256, 128, (4,4), stride=(2,2))
@@ -112,7 +117,7 @@ class UNet(nn.Module):
         # d7=Dropout(0.3)(conv15)
         # conv16 = Conv2D(128,3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(d7)
         # b6=BatchNormalization()(conv16)
-        self.conv2 = DoubleConvBlock(128, 128, 3)
+        self.conv2 = DoubleConvBlock(256, 128, 3)
         
         # conv17 = Conv2DTranspose(128,(4,4), activation = 'relu', padding = 'same',strides=(2,2), kernel_initializer = 'he_normal')(b6)
         self.tconv3 = TConv(128, 64, (4,4), stride=(2,2))
@@ -123,7 +128,7 @@ class UNet(nn.Module):
         # d8=Dropout(0.2)(conv18)
         # conv19 = Conv2D(64,(3,3) ,activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(d8)
         # b7=BatchNormalization()(conv19)
-        self.conv3 = DoubleConvBlock(64, 64, 3)
+        self.conv3 = DoubleConvBlock(128, 64, 3)
 
         # conv20 = Conv2DTranspose(64,(4,4), activation = 'relu', padding = 'same',strides=(2,2), kernel_initializer = 'he_normal')(b7)
         self.tconv4 = TConv(64, 32, (4,4), stride=(2,2))
@@ -133,7 +138,7 @@ class UNet(nn.Module):
         # conv21 = Conv2D(32,(3,3) ,activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(x3)
         # d9=Dropout(0.1)(conv21)
         # conv22 = Conv2D(32,(3,3), activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(d9)
-        self.conv4 = DoubleConvBlock(32, 32, (3,3), batch_norm=False)
+        self.conv4 = DoubleConvBlock(64, 32, (3,3), batch_norm=False)
 
         self.conv5 = nn.Sequential(
             nn.Conv2d(32, 1, (1,1), padding="same"),
@@ -143,55 +148,73 @@ class UNet(nn.Module):
 
     def forward(self, x):
 
+      # print("level 0 channels", x.shape[2])
       # Reduce
       # Outputs 512 x 512 x 32
       layer1 = self.conv(x)
+      # print("level 1 channels", layer1.shape[2])
 
       # Outputs 256 x 256 x 64
-      layer2 = self.pconv1(layer1)
+      skip1, layer2 = self.pconv1(layer1)
+      # print("level 2 channels", layer2.shape[2])
 
       # Outputs 128 x 128 x 128
-      layer3 = self.pconv2(layer2)
+      skip2, layer3 = self.pconv2(layer2)
+      # print("level 3 channels", layer3.shape[2])
 
       # Outputs 64 x 64 x 256
-      layer4 = self.pconv3(layer3)
-
+      skip3, layer4 = self.pconv3(layer3)
+      # print("level 4 channels", layer4.shape[2])
+      
       # Outputs 32 x 32 x 512
-      x = self.pconv4(layer4)
+      skip4, x = self.pconv4(layer4)
+      
+      # print("level 5 channels", x.shape[2])
 
       # Expand
       
       # Outputs 64 x 64 x 256
-      x = self.tconv1(x)
+      print(x.shape, skip3.shape)
+      x = self.tconv1(x, skip3)
+      print("After tconv1", x.shape, skip3.shape)
+      # err
       x = self.conv1(x)
 
+      # print("level 4 channels", x.shape[2])
+
       # Shortcut 4
-      x = x + layer4
+      # print('x2', x.shape)
+      # print('skip5', skip5.shape)
+      # print('skip4', skip4.shape)
+      # print('skip3', skip3.shape)
+      # x = x + skip5
 
       # May need DoubleConvBlock? or shortcut may be best actually
 
       # Outputs 128 x 128 x 128
-      x = self.tconv2(x)
+      x = self.tconv2(x, skip2)
       x = self.conv2(x)
 
       # Shortcut 3
-      x = x + layer3
+      # x = x + skip4
 
       # Outputs 256 x 256 x 64
-      x = self.tconv3(x)
+      x = self.tconv3(x, skip1)
       x = self.conv3(x)
 
       # Shortcut 2
-      x = x + layer2
+      # x = x + skip3
 
       # Outputs 512 x 512 x 32
-      x = self.tconv4(x)
+      x = self.tconv4(x, skip1)
       x = self.conv4(x)
       
       # Shortcut 1
-      x = x + layer1
+      # x = x + skip2
 
       # Outputs 512 x 512 x 1
       x = self.conv5(x)
 
-      return torch.sigmoid(x)
+      # return torch.sigmoid(x)
+      # No sigmoid because using BCEWithLogistLoss which is more numerically stable (using log-sum-exp trick)
+      return x
